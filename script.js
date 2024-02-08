f850bc7518f898bcfe58ea3a63492c9f14ba6698
Hemat Waziri <hematullahw@gmail.com>
2024-02-08 18:47:51 +0430

    JavaScript part complete

--- script.js
+++ script.js
@@ -2,23 +2,64 @@
 // 1 liter = 0.264 gallon 
 // 1 kilogram = 2.204 pound 
 
-function metricToImperial(x) {
-  let toFeet = x * 3.281;
-  let toGallon = x * 0.264;
-  let toPound = x * 2.204;
-  
-  console.log(toFeet, toGallon, toPound);
+const inputElement = document.getElementById("value-input");
+const imperialInput = document.getElementById("to-imperial");
+const metricInput = document.getElementById("to-metric");
+const convertBtn = document.querySelector(".convert-btn");
+const lngValue = document.querySelector(".len-value");
+const massValue = document.querySelector(".mass-value");
+const volValue = document.querySelector(".vol-value");
+
+console.log();
+
+function toImperial(x) {
+    let toFeet = (x * 3.281).toFixed(3);
+    let toPound = (x * 2.204).toFixed(3);
+    let toGallon = (x * 0.264).toFixed(3);
+
+    lngValue.textContent = toFeet;
+    lngValue.nextElementSibling.textContent = "Feets";
+
+    massValue.textContent = toPound;
+    massValue.nextElementSibling.textContent = "Pounds";
+
+    volValue.textContent =  toGallon;
+    volValue.nextElementSibling.textContent = "Gallons";
 }
 
-metricToImperial(5);
 
 
-function imperialToMetric(y) {
-  let toMeter = y / 3.281;
-  let toLiter = y / 0.264;
-  let toKG = y / 2.204;
-  
-  console.log(toMeter, toLiter, toKG)
+function toMetric(y) {
+    let toMeter = (y / 3.281).toFixed(3);
+    let toLiter = (y / 0.264).toFixed(3);
+    let toKG = (y / 2.204).toFixed(3);
+
+
+    lngValue.textContent = toMeter;
+    lngValue.nextElementSibling.textContent = "Meters";
+    
+    massValue.textContent = toLiter;
+    massValue.nextElementSibling.textContent = "Liters";
+
+    volValue.textContent = toKG;
+    volValue.nextElementSibling.textContent = "Kilograms";
+
+
 }
 
-imperialToMetric(10)
+function convert() {
+    let value = inputElement.value;
+    if(value !== "") {
+        if (metricInput.checked) {
+            console.log("to metric")
+            toMetric(value)
+        } else if (imperialInput.checked) {
+            console.log("to imp")
+            toImperial(value)
+        }
+    }
+
+}
+
+convertBtn.addEventListener("click", convert);
+
